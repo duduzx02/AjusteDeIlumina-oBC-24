eeba8f17a47a5324b7c03701ad9e802178ae842d
duduzx02 <duduzx02@gmail.com>
2023-06-22 00:28:51 -0300

    Projeto bora codar 24 'ok'

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-const colorPreview = do
+const colorPreview = document.querySelector(".preview")
+const colorSlider = document.querySelector("#hue-slider")
+const brightnessSlider = document.querySelector("#brightness-slider")
+const contrastSlider = document.querySelector("#contrast-slider")
+
+
+colorSlider.addEventListener("input", (e) => {
+    const hue = e.target.value;
+    const color = `hsl(${hue}, 100%, 50%)`;
+
+    colorPreview.style.backgroundColor = color;
+    colorPreview.style.boxShadow = `0px 0px 53px 13px ${color}`;
+
+})
+
+brightnessSlider.addEventListener("input", (e) => {
+    const brightness = e.target.value;
+
+    colorPreview.style.filter = `brightness(${brightness}%`
+})
+
+contrastSlider.addEventListener("input", (e) => {
+    const contrast = e.target.value;
+
+    colorPreview.style.filter = `contrast(${contrast}%`
+
+})
